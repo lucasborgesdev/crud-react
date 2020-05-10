8d92f2a2268bcfc0c6ee405265090093703fb261
Lucas Borges <45474687+lucasborgesdev@users.noreply.github.com>
2020-05-10 01:26:58 -0300

    finalizado tela de details e rota

--- usuario/src/routes.js
+++ usuario/src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Index from './pages/index/index';
+import Details from './pages/details/details';
 
 
 const Routes = () => (
@@ -8,6 +9,7 @@ const Routes = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path = "/" component={Index} />
+            <Route path = "/usuarios/:id" component={Details} />
         </Switch>
     </BrowserRouter>
 )
